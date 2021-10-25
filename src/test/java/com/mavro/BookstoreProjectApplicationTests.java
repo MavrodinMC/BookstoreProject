a5332de43c15ac18a4206f6770c2b7045f63f319
mihaimavrodin <mihaimavrodin9@gmail.com>
2021-10-25 19:36:31 +0300

    first commit

---
+++ src/test/java/com/mavro/BookstoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
